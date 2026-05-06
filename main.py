4cdcc2a8560465cfffa0b900745a072f186313e7
kar1835 <karthikreddykr443@gmail.com>
2026-05-06 07:06:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from fastapi import FastAPI, UploadFile, File
+import shutil, os, json
+import pandas as pd
+
+app = FastAPI()
+
+os.makedirs("uploads", exist_ok=True)
+os.makedirs("data", exist_ok=True)
+
+@app.get("/")
+def home():
+    return {"message": "KR InvoiceHub Running 🚀"}
+
+
 def extract_data(text):
     try:
         from openai import OpenAI
@@ -17,8 +31,34 @@ basic_fare, other_charges, cgst, sgst, igst, total_amount
             temperature=0
         )
 
-        import json
         return json.loads(response.choices[0].message.content)
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@app.post("/upload/")
+async def upload_invoice(file: UploadFile = File(...)):
+    try:
+        path = f"uploads/{file.filename}"
+
+        with open(path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        text = "invoice data"
+
+        data = extract_data(text)
+
+        file_path = "data/invoices.xlsx"
+        df = pd.DataFrame([data])
+
+        if os.path.exists(file_path):
+            old = pd.read_excel(file_path)
+            df = pd.concat([old, df])
+
+        df.to_excel(file_path, index=False)
+
+        return {"status": "processed", "data": data}
 
     except Exception as e:
         return {"error": str(e)}
