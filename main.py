d99bb000cb0dc1085c26c9413cc74ac10f11718f
kar1835 <karthikreddykr443@gmail.com>
2026-05-06 06:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
 
 app = FastAPI()
+
+if not os.path.exists("uploads"):
+    os.makedirs("uploads")
 
 @app.get("/")
 def home():
     return {"message": "KR InvoiceHub Backend Running 🚀"}
+
+@app.post("/upload/")
+async def upload_invoice(file: UploadFile = File(...)):
+    file_location = f"uploads/{file.filename}"
+    
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"filename": file.filename, "status": "uploaded"}
